e59dc0a0973be5c065d30edbb5c0c0ddf2ecf422
josephadolfomb <josephmobu@gmail.com>
2023-05-30 19:02:06 -0500

    Estilos CSS en React 5/34

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar curso intro react', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALLALALA', completed: false },
+ ];
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
 
-    <TodoCounter />
-    <TodoSearch />
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed={todo.completed} 
+          />
+        ))} 
+      </TodoList>
 
-    <TodoList>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </TodoList>
-
-    <CreateTodoButton />
-      
-    </div>
+      <CreateTodoButton /> 
+    </>
   );
 }
 
